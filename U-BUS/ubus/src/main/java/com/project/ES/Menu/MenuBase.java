4a17139c265eafeed53ea25fcc3845d9fceaa3ef
Antonio Nunes <146894565+AugNuneS@users.noreply.github.com>
2025-06-02 13:07:25 -0300

    added new base menu

--- U-BUS/ubus/src/main/java/com/project/ES/Menu/MenuBase.java
+++ U-BUS/ubus/src/main/java/com/project/ES/Menu/MenuBase.java
@@ -1,18 +1,25 @@
 package com.project.ES.Menu;
+
 import com.project.ES.ES;
+import com.project.DB.DataBaseManager;
 
 public class MenuBase {
-    ES es = new ES();
-    public void limparConsole(){
+
+    public DataBaseManager db = new DataBaseManager("postgres", "1234");
+    public ES es = new ES();
+
+    public void limparConsole() {
         try {
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
         } catch (Exception e) {
-            System.out.println("ERRO: não foi possivel limpar o terminal!");
+            System.out.println("\n╔══════════════════════════════════════════╗");
+            System.out.println("║   ERRO: Nao foi possivel limpar o terminal!   ║");
+            System.out.println("╚══════════════════════════════════════════╝\n");
         }
     }
 
-    public void pausarConsole(){
-        System.out.println("Digite qualquer tecla para continuar!");
+    public void pausarConsole() {
+        System.out.println("\nPressione ENTER para continuar...");
         es.entradaString();
     }
 }
